99c9fa2357afeffde17ed4e026bd85c60d3f83b9
Vitto <mazzavitto1@gmail.com>
2026-05-28 10:49:00 +0200

    Policy, terms, cookies

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+
 import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 import { HeroSection } from "./components/HeroSection";
 import { AboutSection } from "./components/AboutSection";
 import { ServicesSection } from "./components/ServicesSection";
 import { ContactSection } from "./components/ContactSection";
-import { Footer } from "./components/Footer";
-import { Toaster } from "sonner";
+
 import { ServicesPage } from "./pages/ServicesPage";
-import { CustomCursor } from "./components/CustomCursor";
-import ScrollToTop from "./components/ScrollToTop";
-import { CookieBanner } from "./components/CookieBanner";
 import { PrivacyPolicy } from "./pages/PrivacyPolicy";
 import { CookiePolicy } from "./pages/CookiePolicy";
 import { Terms } from "./pages/Terms";
+
+import { CustomCursor } from "./components/CustomCursor";
+import ScrollToTop from "./components/ScrollToTop";
+import { CookieBanner } from "./components/CookieBanner";
+import { Toaster } from "sonner";
 
 function App() {
   return (
@@ -22,15 +25,7 @@ function App() {
       <div className="App">
         <CustomCursor />
         <Header />
-
         <ScrollToTop />
-
-        <Router>
-  <div className="App">
-    <CustomCursor />
-    <Header />
-
-    <ScrollToTop />
 
         <Routes>
           <Route
@@ -51,13 +46,8 @@ function App() {
           <Route path="/termini" element={<Terms />} />
         </Routes>
 
-            <Footer />
-            <CookieBanner />
-            <Toaster />
-          </div>
-        </Router>
-
         <Footer />
+        <CookieBanner />
         <Toaster />
       </div>
     </Router>
